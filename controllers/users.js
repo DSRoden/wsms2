996674fcfd7c699fbe52677611c5560f537360eb
DSRoden <Daniel.S.Roden@gmail.com>
2015-05-05 12:25:42 -0400

    adding prompt engine

--- controllers/users.js
+++ controllers/users.js
@@ -10,15 +10,15 @@ var User = require('../models/user');
 
 exports.questionnaire = function(req, res){
 	User.questionnaire(req.body, function(reply){
-		console.log('reply>>>>>>>>>>', reply);
+		//console.log('reply>>>>>>>>>>', reply);
 		var from = req.body.From;
 		return twilio_post({
 	          to: from,
 	          from: '+18452336666',
 	          body: reply
 	        }).then(function(message) {
-	            console.log('Success sent');
-	            console.log(message);
+	            //console.log('Success sent');
+	            //console.log(message);
 	        });
    });
 };
--- models/user.js
+++ models/user.js
@@ -20,6 +20,7 @@ var personalityInsights = new watson.personality_insights(credentials);
 
 function User(o){
 	this.number = o.From;
+	this.insights = {}; 
 }
 
 Object.defineProperty(User, 'collection', {
@@ -38,29 +39,34 @@ User.questionnaire = function(body, cb){
 	// 	cb();
 	// });
 
-	// User.collection.drop();
 	User.collection.findOne({number: from}, function(err, user){
-	    if(user){
-	    	reply = 'user exists'; 
-	    	var text = "Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the water part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off-then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me. There now is your insular city of the Manhattoes, belted round by wharves as Indian isles by coral reefs-commerce surrounds it with her surf. Right and left, the streets take you waterward. Its extreme  downtown is the battery, where that noble mole is washed by waves, and cooled by breezes, which a few hours previous were out of sight of land. Look at the crowds of water-gazers there.";
-			personalityInsights.profile({text : text}, function(err, profile) {
-		    if (err) {
-		      if (err.message){
-		        err = { error: err.message };
-		        console.log('error in getInsights function', err);
-		      }
-		      cb ('error');
-		    }
-		    else
-		    	console.log('insights>>>>>>>>>>', profile);
-	    		cb(reply);
-		  	});	    	 
-	    } else {
-	    	var user = new User(body);
-	    	reply = 'saving new number';
-	    	User.collection.save(user, cb(reply));
-	  	}
-    });
+		//console.log(user);
+		console.log('insights object>>>>>>>', user.insights);
+		console.log('insights tree>>>>>>>>>>>>>>', user.insights.tree);
+		console.log('insights tree children>>>>>>>>>>>>>>>>>>>', user.insights.tree.children);
+		console.log('insights personality>>>>>>>>>>>>>>>>>>>>>>>>', user.insights.tree.children[0].children);
+		console.log('insights needs>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', user.insights.tree.children[1].children);
+		console.log('insights values>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', user.insights.tree.children[2].children);
+		cb('checking user object');
+	});
+
+	// //User.collection.drop();
+	// User.collection.findOne({number: from}, function(err, user){
+	//     if(user){
+	//     		reply = 'user exists'; 
+	//     		var text = "Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the water part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off-then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me. There now is your insular city of the Manhattoes, belted round by wharves as Indian isles by coral reefs-commerce surrounds it with her surf. Right and left, the streets take you waterward. Its extreme  downtown is the battery, where that noble mole is washed by waves, and cooled by breezes, which a few hours previous were out of sight of land. Look at the crowds of water-gazers there.";
+	// 			getInsights({text: text}, function(insights){
+	// 				user.insights = insights;
+	// 				User.collection.save(user, function(){
+	// 					cb(reply);
+	// 				});
+	// 			});	    	 
+	//     } else {
+	//     	var user = new User(body);
+	//     	reply = 'saving new number';
+	//     	User.collection.save(user, cb(reply));
+	//   	}
+ //    });
 };
 
 
@@ -70,11 +76,8 @@ module.exports = User;
 
 //private functions
 
-function getInsights(req, cb){
-
-	req.text = "Call me Ishmael. Some years ago-never mind how long precisely-having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the water part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off-then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me. There now is your insular city of the Manhattoes, belted round by wharves as Indian isles by coral reefs-commerce surrounds it with her surf. Right and left, the streets take you waterward. Its extreme  downtown is the battery, where that noble mole is washed by waves, and cooled by breezes, which a few hours previous were out of sight of land. Look at the crowds of water-gazers there.";
-
-	 personalityInsights.profile(req.text, function(err, profile) {
+function getInsights(text, cb){
+	personalityInsights.profile(text, function(err, profile) {
     if (err) {
       if (err.message){
         err = { error: err.message };
